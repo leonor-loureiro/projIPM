94c95b12d638a0c48511908165252240b88da504
João Pirralha <joaopirralha@gmail.com>
2017-03-28 16:58:58 +0100

    Setup do protótipo funcional 1, incluíndo bibliotecas (Bootstrap e jQuery).

--- upload_site.sh
+++ upload_site.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-scp site/* ist178375@sigma.tecnico.ulisboa.pt:~/web/IPM
+rsync -avz -e ssh site/* ist178375@sigma.tecnico.ulisboa.pt:~/web/IPM
 echo http://web.tecnico.ulisboa.pt/ist178375/IPM/index.html
