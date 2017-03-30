c92f854eba8ad771cdb412b14ebfc189c87e4db7
João Pirralha <joaopirralha@gmail.com>
2017-03-30 13:49:37 +0100

    Framework de carregamento de sub-ecrãs.
    Funciona o "Fazer pedido".
    
    Nota: no Chromium/Chrome é preciso inicar com
    chromium --allow-file-access-from-files &
    se for testado localmente (com um file:///).

--- site/pf1/main.js
+++ site/pf1/main.js
@@ -1,25 +1,17 @@
-function hideColumns() {
-	var columns = document.getElementsByClassName('col-xs-4'), i;
-	for (var i = 0; i < columns.length; i ++) {
-		columns[i].style.display = 'none';
-	}
-}
-
-function showColumns() {
-	var columns = document.getElementsByClassName('col-xs-4'), i;
-	for (var i = 0; i < columns.length; i ++) {
-		columns[i].style.display = 'inline';
-	}
+function loadMain() {
+	$("#container").load("main.html");
 }
 
 function f1() {
-	hideColumns();
+	$("#container").load("f1_1.html");
+}
+
+function def1() {
+	$("#container").load("main.html");
 }
 
 function f2() {
-	hideColumns();
 }
 
 function f3() {
-	hideColumns();
 }
