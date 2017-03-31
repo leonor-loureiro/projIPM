ca9cdce39e926faef55a5273e354da83efc90aa8
João Pirralha <joaopirralha@gmail.com>
2017-03-31 18:02:15 +0100

    Melhoria no protótipo de selleção de ofertas.

--- site/pf1/f1.js
+++ site/pf1/f1.js
@@ -1,6 +1,37 @@
-function f1_bebidas() {
-	$("#loaded").load("f1_2.html");
+function f1_ofertas(que_oferta) {
+	$("#loaded").load("f1_2.html", function()
+	{
+		$("#ofertas").load(que_oferta);
+	});
 	$("#prato_decor").load("f1_tipos_pratos.html");
+}
+
+function f1_bebidas() {
+	f1_ofertas("f1_2_bebidas.html");
+}
+
+function f1_entradas() {
+	f1_ofertas("f1_2_entradas.html");
+}
+
+function f1_sopas() {
+	f1_ofertas("f1_2_sopas.html");
+}
+
+function f1_carne() {
+	f1_ofertas("f1_2_carne.html");
+}
+
+function f1_peixe() {
+	f1_ofertas("f1_2_peixe.html");
+}
+
+function f1_vegetariano() {
+	f1_ofertas("f1_2_vegetariano.html");
+}
+
+function f1_sobremesas() {
+	f1_ofertas("f1_2_sobremesas.html");
 }
 
 function ofertas_mover_esquerda() {
