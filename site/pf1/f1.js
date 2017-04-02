1adc0e2db7fb259092bd34207b1252e5154bfc82
loureiro_leonor <leonor.filipa@tecnico.ulisboa.pt>
2017-04-02 23:39:11 +0100

    introduzido ecra com informacao nutricional

--- site/pf1/f1.js
+++ site/pf1/f1.js
@@ -14,3 +14,15 @@ function ofertas_mover_esquerda() {
 function ofertas_mover_direita() {
 	document.getElementById("ofertas").scrollLeft += 256;
 }
+
+function f1_info_nutricional(info_produto) {
+	$("#loaded").load("f1_3.html", function()
+	{
+		$("#info_produto").load(info_produto);
+	});
+	("#prato_decor").load("");
+	("#seta_direita").load("");
+	("#seta_esquerda").load("");
+
+}
+
