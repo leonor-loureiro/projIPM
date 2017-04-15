33fa23d520d030126cbd6bdb842fa5fb45e64ad8
loureiro_leonor <leonor.filipa@tecnico.ulisboa.pt>
2017-04-15 17:28:48 +0100

    Design do nome do restaurante melhorado

--- site/pf1/main.js
+++ site/pf1/main.js
@@ -1,5 +1,7 @@
 function loadMain() {
 	$("#loaded").load("main.html");
+	var nome_restaurante = document.getElementById("nome_restaurante");
+	nome_restaurante.style.fontSize = "11.5vw";
 }
 
 function loadModals() {
@@ -7,6 +9,8 @@ function loadModals() {
 }
 
 function f1() {
+	var nome_restaurante = document.getElementById("nome_restaurante");
+	nome_restaurante.style.fontSize = "3.5vw";
 	$("#loaded").load("f1_1.html");
 	$("#prato_decor").load("f1_tipos_pratos.html");
 	$("#area_direita").load("f1_pedido.html");
@@ -14,7 +18,8 @@ function f1() {
 
 function f1_retroceder() {
 	$("#modalNaoSim").modal('hide');
-	$("#loaded").load("main.html");
+//	$("#loaded").load("main.html");
+	loadMain();
 	$("#prato_decor").html("");
 	$("#area_direita").html("");
 }
