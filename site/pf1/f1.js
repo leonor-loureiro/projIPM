d50c04dc943566946ecc0d8eb25ed7751f16077e
Lucy <lucy.hf.c@gmail.com>
2017-04-19 13:05:11 +0100

    Construção do timer no semi-circulo, código está comentado

--- site/pf1/f1.js
+++ site/pf1/f1.js
@@ -193,3 +193,19 @@ function f1_limpar_pedidos() {
 	limpar_pedidos();
 	f1_desenhar_pedidos();
 }
+
+
+
+
+/*var time = 60; /* how long the timer runs for 
+var initialOffset = '440';
+var i = 1
+var interval = setInterval(function() {
+    $('.circle_animation').css('stroke-dashoffset', initialOffset-(i*(initialOffset/time)));
+    $('h2').text(i);
+    if (i == time) {
+        clearInterval(interval);
+    }
+    i++;
+}, 1000);
+*/
